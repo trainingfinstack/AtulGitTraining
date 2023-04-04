3f82551ce9b22e0a40be15c37634610950574071
Atul-tiwari2810 <atul@wowfinstack.com>
2023-04-04 12:38:28 +0100

    add subtract class

---
+++ Subtract.java
@@ -0,0 +1,8 @@
+public class Subtract {
+    public static void main(String args[]){
+        int a=20;
+        int b=10;
+        int c=a-b;
+        System.out.println("after subtraction:-"+c);
+    }
+}
