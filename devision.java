8d50a44c3d8ed5a7b8795b189e53e869e57b2b36
atultiwari <atul@wowfinstack.com>
2023-04-05 10:17:53 +0100

    last commit

---
+++ devision.java
@@ -0,0 +1,8 @@
+public class Devision{
+    public static void main(String args[]){
+        int a=20;
+        int b=4;
+        int c=a/b;
+        System.out.println("two no devision"+c);
+    }
+}
